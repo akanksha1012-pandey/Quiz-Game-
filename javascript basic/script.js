21eddcd11ae0813e790e555ba6066a5fe16f3d64
akanksha1012-pandey <akankshapandey1012@gmail.com>
2020-02-18 22:30:54 +0530

    initial-commit

--- javascript basic/script.js
+++
@@ -1,8 +0,0 @@
-function removeVowelsandspaces(string){
-  var vowels = string.replace(/[aieou\s]+/gi, '');
-  return vowels;
-  
-};
- 
-var mystring = removeVowelsandspaces('my name is akanksha');
-alert(mystring);
